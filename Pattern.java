8a837bb1fb5ac288b81226dd9d50a038cd5ffe24
Akhil Raj <akhilraaaj7@gmail.com>
2023-06-17 11:59:00 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Pattern 
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number of Rows : ");
+        int n = scan.nextInt();
+        for (int i = 1; i<=n;i++){
+          for (int j = i; j<=n; j++) {
+             System.out.print(" "); // decreasing
+          }
+          for (int j = 1; j< i; j++) { // increasing
+             System.out.print(" * ");
+          }
+          for (int j = 1; j<=i; j++) { // increasing
+             System.out.print(" * ");
+          }
+          System.out.println();
+       }
+       for (int i = 1; i<=n ; i++) {
+
+          for (int j = 1; j<=i; j++) { // increasing
+             System.out.print(" ");
+          }
+          for (int j = i; j< n; j++) { // decreasing
+             System.out.print(" * ");
+          }
+          for (int j = i; j<=n; j++) { // decreasing
+             System.out.print(" * ");
+          }
+          System.out.println();
+       } 
+}
+}
